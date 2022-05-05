bf9070f295bf509e753c9c38868cde0db000ab83
yongjies <yongjies@cadence.com>
2022-05-05 14:47:06 +0800

    modifyu

--- UnixLike/linux/Codes/实战linux socket编程--示例源码/Linux_Socket_Programming_By_Example/ch.16/log.h
+++
@@ -1,8 +0,0 @@
-/* log.h
- *
- * log.c externs:
- */
-extern int log_open(const char *pathname);
-extern void log(const char *format,...);
-extern void log_close(void);
-extern void bail(const char *on_what);
